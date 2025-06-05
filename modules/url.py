527210729a6872f56b39b6ef85d16d654388f470
Syed Tahseen <syedtahseen38@gmail.com>
2025-06-05 23:25:05 +0500

    Update url.py

--- modules/url.py
+++ modules/url.py
@@ -1,35 +1,16 @@
-#  Moon-Userbot - telegram userbot
-#  Copyright (C) 2020-present Moon Userbot Organization
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 import asyncio
 import math
 import mimetypes
-
 import os
 import time
 from datetime import datetime
 from io import BytesIO
 from urllib.parse import unquote
-
 import requests
 import urllib3
 from pyrogram import Client, enums, filters
 from pyrogram.types import Message
 from pySmartDL import SmartDL
-
 from utils.config import apiflash_key
 from utils.misc import modules_help, prefix
 from utils.scripts import format_exc, humanbytes, progress
@@ -242,7 +223,7 @@ async def upload_cmd(_, message: Message):
         os.remove(file_name)
 
 
-@Client.on_message(filters.command(["ws", "webshot"], prefix) & filters.me)
+@Client.on_message(filters.command(["ss", "ws", "webshot"], prefix) & filters.me)
 async def webshot(client: Client, message: Message):
     if len(message.command) > 1:
         url = message.text.split(maxsplit=1)[1]
